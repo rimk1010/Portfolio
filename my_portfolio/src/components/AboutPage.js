410218a2231b78be936df862b6099c9a8651a6d5
ryoiwahara <52648669+ryoiwahara@users.noreply.github.com>
2021-01-27 21:25:12 +0900

    Skills,Aboutページ実装
    Skills,Aboutページ実装

--- my_portfolio/src/components/AboutPage.js
+++ my_portfolio/src/components/AboutPage.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, Typography} from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+        maxWidth: 300,
+        margin: 'auto',
+    },
+    media:{
+        height:0,
+        paddingTop:'56.25%',
+    },
+    avatar:{
+        backgroundColor:red[500],
+    },
+}));
+
 
 function AboutPage() {
+    const classes = useStyles();
+
         return (
             <div className='content'>
                 <div className='page-text'>
                     <h1 className='page-title'>ABOUT ME!</h1>
-                    <div className='page-subtitle'>
+                </div>
+                <div className='about-container'>
+                    <div className='about-text'>
                         <h2>SINOBU</h2>
                         <p>WebDeveloperです。よろしくお願いします。</p>
+                    </div>
+                    <div className='about-myself'>
+                        <Card className={classes.root}>
+                            <CardHeader avatar={
+                                <Avatar aria-label='recipe' className={classes.avatar}>S</Avatar>
+                            }
+                            title='SINOBU'
+                            subheader='WEB Developer'
+                            />
+                        <CardMedia className={classes.media} image={`${process.env.PUBLIC_URL}/img/dog.jpg`} title='SINOBU' />
+                        <CardContent>
+                            <Typography variant='body2' color='textSecondary' component='p'>
+                                My name is SINOBU. Im WEB Developer.
+                                Located In Tokyo,
+                                Developer, Engineering, Design.
+                            </Typography>
+                        </CardContent>
+                        </Card>
                     </div>
                 </div>
             </div>
--- my_portfolio/src/components/SkillsPage.js
+++ my_portfolio/src/components/SkillsPage.js
@@ -1,4 +1,34 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+
+const skills = [
+    {
+        name:'HTML',
+        img:'html-5.svg',
+        value:5,
+    },
+    {
+        name:'CSS',
+        img:'css-3.svg',
+        value:4,
+    },
+    {
+        name:'JavaScript',
+        img:'javascript.svg',
+        value:3,
+    },
+    {
+        name:'REACT',
+        img:'react.svg',
+        value:3,
+    },
+    {
+        name:'AWS',
+        img:'aws.svg',
+        value:3,
+    },
+]
 
 function SkillsPage() {
     return (
@@ -8,6 +38,21 @@ function SkillsPage() {
                 <div className='page-subtitle'>
                     <p>スキルページです。</p>
                     <p>できることと熟練度をまとめています。</p>
+                </div>
+                <div className='skills'>
+                    {
+                        skills.map((skill) => {
+                            return (
+                            <div className='skills-container'>
+                                <h3>{skill.name}</h3>
+                                <img width='150' height='150' src={`${process.env.PUBLIC_URL}/img/${skill.img}`} />
+                                <Box component="fieldset" mb={3} borderColor="transparent">
+                                    <Rating size='large' name="read-only" value={skill.value} readOnly />
+                                </Box>
+                            </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
